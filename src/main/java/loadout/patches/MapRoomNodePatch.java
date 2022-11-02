103f1a78a87bd78501d5a38600d994e78b71eb05
Qingqi Wu <qw2341@tc.columbia.edu>
2022-11-01 21:27:59 -0500

    Now gotoanyroom function can be used in combat

--- src/main/java/loadout/patches/MapRoomNodePatch.java
+++ src/main/java/loadout/patches/MapRoomNodePatch.java
@@ -3,10 +3,13 @@ package loadout.patches;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.map.MapRoomNode;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
+import javassist.expr.FieldAccess;
 import javassist.expr.MethodCall;
 import loadout.relics.TildeKey;
 
@@ -20,4 +23,31 @@ public class MapRoomNodePatch {
             else return SpireReturn.Continue();
         }
     }
+
+
+    /**
+     * Used Mercurius from IsaacModExtend
+     */
+    @SpirePatch(
+            clz= MapRoomNode.class,
+            method="update"
+    )
+    public static class RoomPhasePatch {
+        private static AbstractRoom.RoomPhase phase;
+
+        public static void Prefix(MapRoomNode node) {
+            if (TildeKey.canGoToAnyRooms) {
+                phase = AbstractDungeon.getCurrRoom().phase;
+                AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
+            }
+        }
+
+        public static void Postfix(MapRoomNode node) {
+            if (TildeKey.canGoToAnyRooms) {
+                AbstractDungeon.getCurrRoom().phase = phase;
+            }
+        }
+    }
+
+
 }
