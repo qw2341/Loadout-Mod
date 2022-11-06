3a3760627bbc78819a12c705a6c70584cf5ff893
Qingqi Wu <qw2341@tc.columbia.edu>
2022-11-06 14:36:32 -0600

    Added monster button class

--- src/main/java/loadout/screens/MonsterSelectScreen.java
+++ src/main/java/loadout/screens/MonsterSelectScreen.java
@@ -1,6 +1,9 @@
 package loadout.screens;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.megacrit.cardcrawl.helpers.Hitbox;
+import com.megacrit.cardcrawl.localization.MonsterStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 import java.util.ArrayList;
@@ -10,7 +13,17 @@ public class MonsterSelectScreen extends SelectScreen<MonsterSelectScreen.Monste
     public class MonsterButton {
 
         public String id;
+        public String name;
 
+        public String modID;
+
+        public int amount;
+        public Hitbox hb;
+        public float x;
+        public float y;
+        public AbstractMonster.EnemyType type;
+
+        public MonsterStrings monsterStrings;
 
         public MonsterButton() {
         }
