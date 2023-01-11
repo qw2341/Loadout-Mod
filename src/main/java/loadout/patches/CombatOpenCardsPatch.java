d6976e9e315af51f0ad484076695d9363001ba4d
Qingqi Wu <qw2341@tc.columbia.edu>
2023-01-10 18:04:40 -0600

    Fixed Wish's options not being modded

--- src/main/java/loadout/patches/CombatOpenCardsPatch.java
+++ src/main/java/loadout/patches/CombatOpenCardsPatch.java
@@ -4,14 +4,28 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.screens.CardRewardScreen;
+import com.megacrit.cardcrawl.screens.CombatRewardScreen;
 import loadout.savables.CardModifications;
 
 import java.util.ArrayList;
 
-@SpirePatch(clz = CardRewardScreen.class, method = "customCombatOpen", paramtypez = {ArrayList.class, String.class, boolean.class})
+
 public class CombatOpenCardsPatch {
-    @SpirePrefixPatch
-    public static void Prefix(CardRewardScreen __instance, ArrayList<AbstractCard> choices, String text, boolean skippable) {
-        for (AbstractCard card: choices) CardModifications.modifyOnlyNumberIfExist(card);
+    @SpirePatch(clz = CardRewardScreen.class, method = "customCombatOpen", paramtypez = {ArrayList.class, String.class, boolean.class})
+    public static class CombatOpenPatch {
+        @SpirePrefixPatch
+        public static void Prefix(CardRewardScreen __instance, ArrayList<AbstractCard> choices, String text, boolean skippable) {
+            for (AbstractCard card: choices) CardModifications.modifyOnlyNumberIfExist(card);
+        }
     }
+
+    @SpirePatch(clz = CardRewardScreen.class, method = "chooseOneOpen", paramtypez = {ArrayList.class})
+    public static class ChooseOnePatch {
+        @SpirePrefixPatch
+        public static void Prefix(CardRewardScreen __instance, ArrayList<AbstractCard> choices) {
+            for(AbstractCard card: choices) CardModifications.modifyOnlyNumberIfExist(card);
+        }
+
+    }
+
 }
