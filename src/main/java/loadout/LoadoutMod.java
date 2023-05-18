1da3c4a1e6f338cefcdca79941cccacaefe3b59d
Qingqi Wu <qw2341@tc.columbia.edu>
2023-05-18 01:27:15 -0700

    Added compatibility with Fabricate

--- src/main/java/loadout/LoadoutMod.java
+++ src/main/java/loadout/LoadoutMod.java
@@ -54,6 +54,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import loadout.relics.*;
 import org.clapper.util.classutil.*;
+import pinacolada.cards.base.PCLCustomCardSlot;
 import relicupgradelib.arch.Proxy;
 import relicupgradelib.arch.ProxyManager;
 import relicupgradelib.arch.UpgradeBranch;
@@ -1150,15 +1151,11 @@ StartGameSubscriber, PrePlayerUpdateSubscriber, RenderSubscriber, PostCampfireSu
         }
         if(Loader.isModLoaded("pinacolada-fabricate")){
             logger.info("Fabricate detected, adding custom cards");
-//            Collection<PCLCardData> pclCards = PCLCardData.getAllData(true, false, (extendedui.interfaces.delegates.FuncT1)null);
-//            pclCards.forEach(pclCardData -> {
-//                PCLCard pclCard = pclCardData.create(0,0);
-//                cardsToDisplay.add(pclCard);
-//            });
+//
             try{
-                pinacolada.cards.base.PCLCustomCardSlot.getCards(null).forEach(slot -> {
+                ArrayList<PCLCustomCardSlot> customCards = pinacolada.cards.base.PCLCustomCardSlot.getCards(null);
+                for (PCLCustomCardSlot slot : customCards)
                     cardsToDisplay.add(slot.getBuilder(0).create(0));
-                });
             } catch (RuntimeException e) {
                 logger.info("Failed to add Fabricate custom cards");
             }
