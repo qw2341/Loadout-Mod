efd1ed37528744626807927e4251094216840f21
Qingqi Wu <qw2341@tc.columbia.edu>
2023-08-20 10:23:58 -0700

    Actually fixed the crashing issue

--- src/main/java/loadout/patches/blightfixes/BlightFixes.java
+++ src/main/java/loadout/patches/blightfixes/BlightFixes.java
@@ -5,11 +5,14 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatches;
 import com.evacipated.cardcrawl.modthespire.lib.SpireRawPatch;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.rooms.MonsterRoomElite;
+import javassist.CannotCompileException;
 import javassist.CtBehavior;
 import javassist.bytecode.*;
+import javassist.expr.ExprEditor;
 import loadout.LoadoutMod;
 
 import java.util.Arrays;
@@ -23,36 +26,14 @@ import java.util.Arrays;
         @SpirePatch(clz = AbstractCreature.class, method = "heal", paramtypez = {int.class, boolean.class}),
 @SpirePatch(clz = DamageInfo.class, method = "applyPowers", paramtypez = {AbstractCreature.class, AbstractCreature.class})})
 public class BlightFixes {
-    @SpireRawPatch
-    public static void Raw(CtBehavior ctMethodToPatch) throws BadBytecode {
-        CodeAttribute codeAttr = ctMethodToPatch.getMethodInfo().getCodeAttribute();
-        CodeIterator iterator = codeAttr.iterator();
-        ConstPool constPool = ctMethodToPatch.getDeclaringClass().getClassFile2().getConstPool();
-        boolean success = false;
-        while (iterator.hasNext()) {
-            int pos = iterator.next();
-            int opcode = iterator.byteAt(pos);
-
-            if (opcode == Opcode.GETSTATIC) {
-                int fieldIndex = iterator.u16bitAt(pos + 1);
-                String fieldName = constPool.getFieldrefName(fieldIndex);
-                if ("isEndless".equals(fieldName)) {
-
-                    byte[] nops = new byte[6];  // Assuming 6 bytes total for GETSTATIC and IFEQ together.
-                    Arrays.fill(nops, (byte) Opcode.NOP);
-                    iterator.write(nops, pos);
-
-                    success = true;
-                    break;
+    public static ExprEditor Instrument()
+    {
+        return new ExprEditor() {
+            public void edit(javassist.expr.FieldAccess m) throws CannotCompileException {
+                if (m.getClassName().equals(Settings.class.getName()) && m.getFieldName().equals("isEndless")) {
+                    m.replace("{$_ = com.megacrit.cardcrawl.dungeons.AbstractDungeon.player != null;}");
                 }
             }
-        }
-
-        if(success) {
-            LoadoutMod.logger.info("{} successfully patched!", ctMethodToPatch.getName());
-            ctMethodToPatch.getMethodInfo().rebuildStackMap(ctMethodToPatch.getDeclaringClass().getClassPool());
-        } else {
-            LoadoutMod.logger.warn("{} patch failed!",  ctMethodToPatch.getName());
-        }
+        };
     }
 }
