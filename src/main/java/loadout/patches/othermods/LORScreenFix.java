ffc82a188711185cc6ea2c28a1631afe7b3237d0
Qingqi Wu <qw2341@tc.columbia.edu>
2024-03-26 19:26:21 +0800

    Updated with new LOR version

--- src/main/java/loadout/patches/othermods/LORScreenFix.java
+++ src/main/java/loadout/patches/othermods/LORScreenFix.java
@@ -5,11 +5,11 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import loadout.LoadoutMod;
-import lor.Utils;
+import lor.LORUtils;
 
 import java.util.ArrayList;
 
-@SpirePatch(clz = Utils.class, method = "openCardRewardsScreen", paramtypez = {ArrayList.class, boolean.class, int.class, String.class}, requiredModId = "Library of Ruina", optional = true)
+@SpirePatch(clz = LORUtils.class, method = "openCardRewardsScreen", paramtypez = {ArrayList.class, boolean.class, int.class, String.class}, requiredModId = "Library of Ruina", optional = true)
 public class LORScreenFix {
     public static void Prefix(ArrayList<AbstractCard> cards, boolean allowSkip, int appearnum, String text) {
         LoadoutMod.isScreenUp = true;
