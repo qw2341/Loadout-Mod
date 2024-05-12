1bff54fac04d4cc52efde1c093fd2989a926da28
Jason <qwu86@wisc.edu>
2024-05-12 23:32:36 +0800

    Reworked how go to any normal rooms work

--- src/main/java/loadout/patches/MapRoomNodePatch.java
+++ src/main/java/loadout/patches/MapRoomNodePatch.java
@@ -1,13 +1,19 @@
 package loadout.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.controller.CInputActionSet;
+import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import com.megacrit.cardcrawl.map.DungeonMap;
 import com.megacrit.cardcrawl.map.MapRoomNode;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import javassist.CannotCompileException;
+import javassist.CtBehavior;
+import javassist.expr.Cast;
 import javassist.expr.ExprEditor;
 import javassist.expr.FieldAccess;
 import javassist.expr.MethodCall;
@@ -15,39 +21,45 @@ import loadout.relics.TildeKey;
 
 public class MapRoomNodePatch {
 
-    @SpirePatch(clz = MapRoomNode.class, method = "isConnectedTo", paramtypez = {MapRoomNode.class})
-    public static class ConnectionPatch {
-        @SpirePrefixPatch
-        public static SpireReturn<Boolean> Prefix(MapRoomNode __Instance, MapRoomNode node) {
-            if(TildeKey.canGoToAnyRooms) return SpireReturn.Return(true);
-            else return SpireReturn.Continue();
+    @SpirePatch2(clz = MapRoomNode.class, method = "update")
+    public static class NodeFakeDebugPatch {
+        public static void Raw(CtBehavior ctMethodToPatch) throws CannotCompileException {
+            ctMethodToPatch.instrument(new ExprEditor() {
+                @Override
+                public void edit(FieldAccess f) throws CannotCompileException {
+                    if(f.getFieldName().equals("isDebug")) {
+                        f.replace("$_ = ($proceed() || loadout.relics.TildeKey.canGoToAnyRooms);");
+                    }
+                }
+
+                @Override
+                public void edit(MethodCall m) throws CannotCompileException {
+                    if (m.getClassName().equals(AbstractRoom.RoomPhase.class.getName()) &&
+                    m.getMethodName().equals("equals")) {
+                        m.replace("$_ = ($proceed($$) || loadout.relics.TildeKey.canGoToAnyRooms);");
+                    }
+                }
+            });
         }
     }
 
+    @SpirePatch2(clz = DungeonMap.class, method = "update")
+    public static class MapPatch {
+        public static void Raw(CtBehavior ctMethodToPatch) throws CannotCompileException {
+            ctMethodToPatch.instrument(new ExprEditor() {
+                @Override
+                public void edit(FieldAccess f) throws CannotCompileException {
 
-    /**
-     * Used Mercurius from IsaacModExtend
-     */
-    @SpirePatch(
-            clz= MapRoomNode.class,
-            method="update"
-    )
-    public static class RoomPhasePatch {
-        private static AbstractRoom.RoomPhase phase;
+                    if(f.getClassName().equals(AbstractRoom.class.getName())
+                            && f.getFieldName().equals("phase")) {
+                        f.replace("$_ = loadout.relics.TildeKey.canGoToAnyRooms ? com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase.COMPLETE : $proceed();");
+                    } else if (f.getFieldName().equals("isDebug")) {
+                        f.replace("$_ = ($proceed() || loadout.relics.TildeKey.canGoToAnyRooms);");
+                    }
+                }
 
-        public static void Prefix(MapRoomNode node) {
-            if (TildeKey.canGoToAnyRooms) {
-                phase = AbstractDungeon.getCurrRoom().phase;
-                AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
-            }
-        }
-
-        public static void Postfix(MapRoomNode node) {
-            if (TildeKey.canGoToAnyRooms) {
-                AbstractDungeon.getCurrRoom().phase = phase;
-            }
+            });
         }
     }
-
 
 }
