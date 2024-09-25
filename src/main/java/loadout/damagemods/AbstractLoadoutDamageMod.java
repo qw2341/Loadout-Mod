ee608d1a364300f02fc83aed139a32a473464afc
Jason <qwu86@wisc.edu>
2024-09-26 03:07:04 +0800

    Fixed #9

--- src/main/java/loadout/damagemods/AbstractLoadoutDamageMod.java
+++ src/main/java/loadout/damagemods/AbstractLoadoutDamageMod.java
@@ -6,13 +6,27 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.colorless.Madness;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.RegrowPower;
 import loadout.patches.AbstractCardPatch;
+import loadout.util.Wiz;
 
 public abstract class AbstractLoadoutDamageMod extends AbstractDamageModifier {
 
     protected boolean isValidKill(DamageInfo info, int lastDamageTaken, AbstractCreature targetHit) {
         return DamageModifierManager.getInstigator(info) instanceof com.megacrit.cardcrawl.cards.AbstractCard &&
-                targetHit.currentHealth > 0 && targetHit.currentHealth - lastDamageTaken <= 0 && !targetHit.halfDead && !targetHit.hasPower("Minion");
+                targetHit.currentHealth > 0 && targetHit.currentHealth - lastDamageTaken <= 0 && !targetHit.halfDead && !targetHit.hasPower("Minion") && isOnlyLifeLinkLeft(targetHit);
+    }
+
+    protected boolean isOnlyLifeLinkLeft(AbstractCreature targetHit) {
+        if(!targetHit.hasPower(RegrowPower.POWER_ID)) return true;
+
+        final boolean[] ret = {true};
+        Wiz.getEnemies().forEach((m)-> {
+            if(m != targetHit && !m.halfDead) {
+                if (m.hasPower(RegrowPower.POWER_ID)) ret[0] = false;
+            }
+        });
+        return ret[0];
     }
 
     protected boolean isValid(DamageInfo info, AbstractCreature targetHit) {
