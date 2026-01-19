d9e767c52a7c8cd5c7ed4e9079c0b3fa74d78080
qw2341 <qw2341@tc.columbia.edu>
2026-01-20 00:04:50 +0800

    Rewrote multiuse to make the X modifier more consistent

--- src/main/java/loadout/actions/MultiUseAction.java
+++ src/main/java/loadout/actions/MultiUseAction.java
@@ -32,11 +32,9 @@ public class MultiUseAction extends AbstractGameAction {
             isDone = true;
 
             if (usesLeft > 0 && card.canUse(player, monster)) {
-//                card.calculateCardDamage(monster);
-                card.use(player, monster);
-                if (usesLeft > 1) {
-                    AbstractDungeon.actionManager.addToBottom(new MultiUseAction(card, player, monster, usesLeft - 1));
-                }
+                for (int i=0; i < usesLeft; i++)
+                    card.use(player, monster);
+
             }
         }
     }
